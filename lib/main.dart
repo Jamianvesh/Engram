3671b4c3ecd966514cb23428f73a3e86bc460c3f
Anvesh <anveshjami2006@gmail.com>
2025-09-06 12:15:38 +0530

    Fixed login persistence issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:pro/theme.dart';
 
 import 'firebase/firebase_options.dart';
 import 'routes.dart';
+import 'theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,8 +21,38 @@ class MyApp extends StatelessWidget {
       title: 'Quality English',
       debugShowCheckedModeBanner: false,
       theme: buildTheme(),
-      initialRoute: '/',
       routes: appRoutes,
+      home: const AuthWrapper(),
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        if (snapshot.hasData) {
+          return Navigator(
+            onGenerateRoute: (settings) => MaterialPageRoute(
+              builder: (_) => appRoutes['/welcome']!(context),
+            ),
+          );
+        } else {
+          return Navigator(
+            onGenerateRoute: (settings) =>
+                MaterialPageRoute(builder: (_) => appRoutes['/']!(context)),
+          );
+        }
+      },
     );
   }
 }
